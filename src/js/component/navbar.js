4a47fbd11dda635eeecb23bba55c46a2f3b2ea6e
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-02-27 20:07:28 +0000

    Added Star Wars logo and 'Favorite' dropdown menu to Navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-light bg-light mb-3">
+            <Link to="/">
+                <img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" alt="Star Wars Logo" style={{ width: "150px", height: "auto" }} />
+            </Link>
+            <div className="ml-auto">
+                <Link to="/demo">
+				<button class="btn btn-primary btn-large dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Favorites
+  </button>
+                </Link>
+            </div>
+        </nav>
+    );
 };
